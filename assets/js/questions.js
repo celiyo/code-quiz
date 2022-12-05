46ed478cc6a11267c258a0d2b002a8a3e2383b2e
celiyo <riana.tysson@gmail.com>
2022-12-05 21:31:34 +0000

    Add quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+var questions = [
+  {
+    id: 1,
+    title: "Which of these is NOT an animal of the Chinese Zodiac?",
+    choices: ["Rooster", "Monkey", "Pig", "Elephant"],
+    answer: "Elephant",
+  },
+  {
+    id: 2,
+    title: "What two animals make up Australian coat of arms?",
+    choices: [
+      "Wombat & wallaby",
+      "Snake & spider",
+      "Kangaroo & emu",
+      "Dragon & dingo",
+    ],
+    answer: "Kangaroo & emu",
+  },
+  {
+    id: 3,
+    title: "When cooked, which animal becomes 'fugu', a delicacy in Japan?",
+    choices: ["Shrimp", "Pufferfish", "Shark", "Eel"],
+    answer: "Pufferfish",
+  },
+  {
+    id: 4,
+    title: "Ocelots live mainly on which continent?",
+    choices: ["Africa", "Asia", "Europe", "South America"],
+    answer: "South America",
+  },
+  {
+    id: 5,
+    title: "Someone with 'musophobia' suffers from a fear of which animal?",
+    choices: ["Meerkats", "Elephants", "Mice", "Ostriches"],
+    answer: "Mice",
+  },
+  {
+    id: 6,
+    title:
+      "Which animal has the longest tongue in relation to its body length?",
+    choices: ["Anteater", "Chameleon", "Sun bear", "Hummingbird"],
+    answer: "Chameleon",
+  },
+];
